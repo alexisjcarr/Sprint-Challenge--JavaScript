898da8925c7a73551d0a79e6a3c99c3d229f6da7
alexisjcarr <acarr59@gmail.com>
2019-05-10 11:17:23 -0500

    functions.js completed

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,18 +7,35 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
+const consume = (param1, param2, cb) => {
+  return cb(param1, param2);
+};
+
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
-  * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
+  * Create a function named greeting that accepts a first and last name and returns 
+  * "Hello first-name last-name, nice to meet you!"
 */
+
+const add = (num1, num2) => {
+  return num1 + num2;
+};
+
+const multiply = (num1, num2) => {
+  return num1 * num2;
+};
+
+const greeting = (first, last) => {
+  return `Hello ${first} ${last}, nice to meet you!`
+}
 
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2,2,add)); // 4
+console.log(consume(10,16,multiply)); // 160
+console.log(consume("Mary","Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
@@ -26,6 +43,9 @@
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
 // Explanation: 
+/* `nestedfunction() can access the variable `internal`, because closure allows for 
+functions to "grab" variables outside of their brackets.
+*/
 
 
 const external = "I'm outside the function";
